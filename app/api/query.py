b4b1a890fe2447f7d2f6bc5ea7126d3d5dfa9e82
ethanjs02 <itsethanjs@gmail.com>
2024-11-20 09:35:00 -0600

    refactor: refactored routes to handle player data

--- app/api/query.py
+++ app/api/query.py
@@ -6,25 +6,54 @@ from dotenv import load_dotenv
 #Load environment variables
 load_dotenv()
 
-def get_nfl_team_data():
-    url = "https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data"
-
-    headers = {
+headers = {
         "x-rapidapi-key": os.getenv("API_KEY"),
         "x-rapidapi-host": "nfl-api-data.p.rapidapi.com"
-    }
-    
-    try:
-        response = requests.get(url, headers=headers, verify=certifi.where())
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
+    }       
 
-if __name__ == "__main__":
-    data = get_nfl_team_data()
-    if data:
-        print(data)
-    else:
-        print("Failed to retrieve NFL team data.")
+def get_nfl_player_data(player_id, team_id, pos_id):
+    if(pos_id == "8"):
+        return get_qb_stats(player_id, team_id)
+    elif(pos_id in ["1", "7"] ):
+        return get_wr_stats(player_id, team_id)
+    elif(pos_id == "9"):
+        return get_rb_stats(player_id, team_id)
+    elif(pos_id == "22"):
+        return get_k_stats(player_id, team_id)
+    
+
+def get_qb_stats(player_id, team_id):
+    None
+
+def get_wr_stats(player_id, team_id):
+    None
+
+def get_rb_stats(player_id, team_id):
+    None
+
+def get_k_stats(player_id, team_id):
+    None
+
+
+def get_player_stats(player_id):
+    None
+
+def get_team_stats(team_id):
+    None
+
+def get_schedule(team_id):
+    None    
+    
+    
+    # url = "https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data"
+    
+    # # querystring = {"id":"15035","year":"2024"}
+    # querystring = {"id": 25}
+    
+    # try:
+    #     response = requests.get(url, headers=headers, params=querystring, verify=certifi.where())
+    #     response.raise_for_status()
+    #     return response.json()
+    # except requests.RequestException as e:
+    #     print(f"An error occurred: {e}")
+    #     return None
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,9 @@
 #app/api/routes.py
 from fastapi import APIRouter
-from .query import get_nfl_team_data
+from .query import get_nfl_team_data, get_nfl_player_data, get_schedule_by_team
 
 router = APIRouter()
 
-@router.get("/nfl-team-data")
-async def get_data():
-    return get_nfl_team_data()
+@router.get("/player_data")
+async def get_player_data(player_id: str, team_id: str, pos_id: str):
+    return get_nfl_player_data(player_id, team_id, pos_id)
